db96e997e8f6799634b4bde6e38848168465b16c
RafaellaSSouza <rafaella1403@hotmail.com>
2022-05-03 23:04:50 -0300

    Create
    index

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="h-full">
+    <Header/>
+    <Main/>
+  </div>
+</template>
+
+<script>
+import Header from "~/components/organismos/Header.vue";
+import Main from "~/components/organismos/Main.vue";
+
+export default {
+    components: { Header, Main }
+};
+</script>
+
+<style>
+ .container { 
+    @apply flex flex-col flex-wrap items-center mt-5
+  }
+</style>

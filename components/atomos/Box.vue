b39a8ed8d7181dbe5311a0b9f410cc6b399e8bd1
RafaellaSSouza <rafaella1403@hotmail.com>
2022-05-04 23:25:31 -0300

    Create
    Componente box

---
+++ components/atomos/Box.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="border-2 border-cor3 rounded-lg w-20 h-full bg-branco">
+       <img class="w-10" :src="logo" alt="logo">
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        logo: '../../static/html.png'
+    }
+    
+}
+</script>

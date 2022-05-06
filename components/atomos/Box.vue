c8297dbc4a5847106dcbb2388311d2ce18cbe668
RafaellaSSouza <rafaella1403@hotmail.com>
2022-05-05 23:19:26 -0300

    Update
    melhorias

--- components/atomos/Box.vue
+++ components/atomos/Box.vue
@@ -1,14 +1,29 @@
 <template>
-    <div class="border-2 border-cor3 rounded-lg w-20 h-full bg-branco">
-       <img class="w-10" :src="logo" alt="logo">
+    <div class="box">
+       <img :src="logo" alt="logo">
     </div>
 </template>
 
 <script>
 export default {
     props: {
-        logo: '../../static/html.png'
+        logo: {
+            type: String,
+            default: () => ""
+        }
     }
-    
 }
 </script>
+
+<style scoped>
+    .box {
+        width: 15vh;
+        height: 15vh;
+        @apply
+        p-2
+        border-2 border-cor3 
+        rounded-lg  
+        bg-branco
+        hover:opacity-50
+    }
+</style>

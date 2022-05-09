1e256ef882e5040d7932940834631826d7873e36
RafaellaSSouza <rafaella1403@hotmail.com>
2022-05-09 18:43:14 -0300

    Create
    Configs

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,9 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'project-nuxt',
+    title: 'Portfólio | Rafaella Souza',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'pt-BR'
     },
     meta: [
       { charset: 'utf-8' },
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,31 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    
+    extend: {
+      colors: {
+        cor1: '#18184e',
+        cor2: '#232752',
+        branco: 'rgb(255 255 255)',
+        cor3: '#55cba9',
+        cor5: '#A9A9A9'
+      },
+      keyframes: {
+        float: {
+          '0%': { transform: 'translatey(0px)' },
+          '50%': { transform: 'translatey(-30px)' },
+          '100%': { transform: 'translatey(0px)' }
+        }
+      },
+      animation: {
+        float: 'float 5s ease-in-out infinite',
+      },
+      
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    variants: {
+      extend: {
+
+      },
+    },
+    plugins: [],
+  }
 }
